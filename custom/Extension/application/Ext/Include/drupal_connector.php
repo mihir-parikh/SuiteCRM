49a55cff0a4db838cd3236ce95a7fa6329293bd8
Mihir Parikh <mihir1408@gmail.com>
2019-05-24 08:42:00 +1000

    Removal of redundant dc_drupal_connector module

--- custom/Extension/application/Ext/Include/drupal_connector.php
+++
@@ -1,7 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['dc_drupal_connector'] = 'dc_drupal_connector';
-$beanFiles['dc_drupal_connector'] = 'modules/dc_drupal_connector/dc_drupal_connector.php';
-$moduleList[] = 'dc_drupal_connector';
-
-?>
--- custom/modules/unified_search_modules_display.php
+++ custom/modules/unified_search_modules_display.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2018-08-15 11:18:10
+// created: 2019-05-24 00:38:21
 $unified_search_modules_display = array (
   'Accounts' => 
   array (

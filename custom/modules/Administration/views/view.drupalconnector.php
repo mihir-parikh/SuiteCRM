f69715bf482b58fb318c769923dbdff28062f61e
Mihir Parikh <mihir1408@gmail.com>
2019-05-29 20:01:26 +1000

    v0.1 of decrypt_password

--- custom/modules/Administration/views/view.drupalconnector.php
+++ custom/modules/Administration/views/view.drupalconnector.php
@@ -55,12 +55,24 @@ class ViewDrupalConnector extends \SugarView {
 			$smarty->assign('DRUPAL_USERNAME', $admin->settings['drupal_connector_drupal_username']);
 		}
 
+		// Decrypt the password
+
 		if (array_key_exists('drupal_connector_drupal_password', $admin->settings)) {
 			$smarty->assign('DRUPAL_PASSWORD', $admin->settings['drupal_connector_drupal_password']);
 		}
 
 		// Display the custom tpl
 		$smarty->display($this->dv->tpl);
+	}
+
+	/**
+	 * TODO: Write PHPDoc
+	 */
+	private function decrypt_password($admin) {
+		$crypted_token = $admin->settings['drupal_connector_drupal_password'];
+		list($crypted_token, $enc_iv) = explode("::", $crypted_token);
+		$cipher_method = 'aes-128-ctr';
+		$enc_key = $admin->settings['drupal_connector_drupal_enc_key'];
 	}
 }
 
